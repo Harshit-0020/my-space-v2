8c088616ca67584000d86ac2a7e58668efe4b827
Harshit <harshitbhardwaj015@gmail.com>
2024-11-30 05:49:54 +0400

    Toggle v1 added

---
+++ js/theme-switcher.js
@@ -0,0 +1,11 @@
+  document.getElementById('theme-toggle').addEventListener('click', function() {
+    var link = document.getElementById('theme-style');
+    
+    // Check if the current stylesheet is the default one
+    if (link.getAttribute('href') === '../css/colorschemes/dark.css') {
+      link.setAttribute('href', '../css/colorschemes/light.css');  // Switch to dark.css
+    } else {
+      link.setAttribute('href', '../css/colorschemes/dark.css');  // Switch back to light.css
+    }
+  });
+
