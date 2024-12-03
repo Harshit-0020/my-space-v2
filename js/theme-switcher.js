8fc78c1aad1f8ba17a104f311ee4e54dca7b5bd6
Harshit <harshitbhardwaj015@gmail.com>
2024-12-03 04:27:18 +0400

    Working bulb added. However is not as good looking as expected

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,11 +1,11 @@
-  document.getElementById('themeToggle').addEventListener('click', function() {
-    var link = document.getElementById('theme-style');
-    
-    // Check if the current stylesheet is the default one
-    if (link.getAttribute('href') === '../css/colorschemes/dark.css') {
-      link.setAttribute('href', '../css/colorschemes/light.css');  // Switch to dark.css
-    } else {
-      link.setAttribute('href', '../css/colorschemes/dark.css');  // Switch back to light.css
-    }
-  });
+document.getElementById("bulb").addEventListener('click', function() {
+	var link = document.getElementById('theme-style');
+	console.log("Cick detected");
 
+	// Check if the current stylesheet is the default one
+	if (link.getAttribute('href') === '../css/colorschemes/dark.css') {
+		link.setAttribute('href', '../css/colorschemes/light.css');  // Switch to dark.css
+	} else {
+		link.setAttribute('href', '../css/colorschemes/dark.css');  // Switch back to light.css
+	}
+});
